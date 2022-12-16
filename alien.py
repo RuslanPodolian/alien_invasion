b7bc61165fe0de3f11e603fffcfc674fe556e8f9
RuslanPodolian <Ruslan.podolian@gmail.com>
2022-12-16 22:53:58 +0200

    test commit

--- alien.py
+++ alien.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.sprite import Sprite
 
 class Alien(Sprite):
+    # Hi I'm alien
     def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
